39ed39f3816d1def424d23c9f24ee6096cb51408
hongbi-lee <foxgirin950@gmail.com>
2021-10-02 17:17:43 +0900

    do moo game

--- week3/moo_game.py
+++ week3/moo_game.py
@@ -13,20 +13,50 @@
     - 출력: Moo 수열의 N번째 글자
     - 11 입력시, m 출력
 """
+import copy
 
 
-def mk_moo_seq(num):
-    moo_seq = []
-    s0 = ['m', 'o', 'o']
+def mk_moo(num):
+    moo_seq = ['m']
     if num == 0:
-        return s0
+        return ['m', 'o', 'o']
+    for i in range(num+2):
+        moo_seq.append('o')
+        i += 1
+    return moo_seq
+
+
+def mk_seq(num, seq1, seq2):
+    seq1.extend(mk_moo(num))
+    seq1.extend(seq2)
+    return seq1
+
+
+def moo_sequence():
+    seq = mk_moo(0)
+    s0 = mk_moo(0)
+    mk_seq(1, seq, s0)  # seq 생성
+    s1 = copy.deepcopy(seq)
+    mk_seq(2, seq, s1)  # seq 생성
+    s2 = copy.deepcopy(seq)
+    mk_seq(3, seq, s2)  # seq 생성
+    s3 = copy.deepcopy(seq)
+    mk_seq(4, seq, s3)
+    return seq
+
+
+def moo(num):
+    if num == 0:
+        return 'moo'
     else:
-        for i in num:
-            continue
-    return
+        return moo(num-1) + 'm' + 'o' * (num+2) + moo(num-1)
 
 
-def moo_game():
-    n = int(input())
+if __name__ == '__main__':
+    num = int(input())
 
-    moo_list = ['m', 'o', 'o']
+    # 첫 번째
+    # print(moo_sequence()[num-1])
+
+    # 두 번째
+    print(moo(4)[num-1])

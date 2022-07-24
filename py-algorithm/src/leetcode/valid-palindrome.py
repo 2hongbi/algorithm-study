21542e6a4940edc41bab98a9711d91b5fea50150
hongbi-lee <foxgirin950@gmail.com>
2022-07-25 01:00:07 +0900

    solve valid-palindrome

--- py-algorithm/src/leetcode/valid-palindrome.py
+++ py-algorithm/src/leetcode/valid-palindrome.py
@@ -1,3 +1,8 @@
+import collections
+import re
+from typing import Deque
+
+
 def is_palindrome1(s):
     # use list
     strs = []
@@ -12,6 +17,31 @@ def is_palindrome1(s):
     return True
 
 
+def is_palindrome2(s: str) -> bool:
+    # declare deque as data type
+    strs: Deque = collections.deque()
+
+    for char in s:
+        if char.isalnum():
+            strs.append(char.lower())
+
+    while len(strs) > 1:
+        if strs.popleft() != strs.pop():
+            return False
+
+    return True
+
+
+def is_palindrome3(s: str) -> bool:
+    # using slicing
+    s = s.lower()
+    # remove unnecessary character
+    s = re.sub('[^a-z0-9]', '', s)
+    return s == s[::-1]  # slicing
+
+
 if __name__ == '__main__':
     s = "A man, a plan, a canal: Panama"
     print(is_palindrome1(s))
+    print(is_palindrome2(s=s))
+    print(is_palindrome3(s))
